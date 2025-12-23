63a4987560e4335ce5c17a9a2ff2e09a08e946f3
Kevin Santamaria <kevinsdasere@gmail.com>
2025-12-23 14:52:40 -0500

    feat(frontend): create LoginPage Component

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,103 @@
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@radix-ui/react-label'
+import { IconEye, IconEyeClosed } from '@tabler/icons-react'
+import { type FormEvent, useState } from 'react'
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
+
+  const togglePasswordVisibility = () => setShowPassword(!showPassword)
+
+  const handleLogin = async (e: FormEvent) => {
+    e.preventDefault()
+  }
+
+  return (
+    <div className="flex min-h-screen w-screen  bg-background overflow-hidden">
+      <div className="flex-1 flex items-center justify-center p-4">
+        <form onSubmit={handleLogin}>
+          <Card className="w-full max-w-md">
+            <CardHeader>
+              <CardTitle>Iniciar sesión</CardTitle>
+              <CardDescription>
+                Ingresa tus credenciales para acceder al panel de administración
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="space-y-4">
+                <div className="space-y-2">
+                  <Label htmlFor="email">Nombre de usuario</Label>
+                  <Input
+                    id="email"
+                    placeholder="username"
+                    required
+                    name="email"
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </div>
+                <div className="space-y-2">
+                  <Label htmlFor="password">Contraseña</Label>
+                  <div className="relative">
+                    <Input
+                      id="password"
+                      type={showPassword ? 'text' : 'password'}
+                      placeholder="••••••••"
+                      required
+                      name="password"
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <Button
+                      type="button"
+                      variant="ghost"
+                      size="icon"
+                      className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
+                      onClick={togglePasswordVisibility}
+                    >
+                      {showPassword ? (
+                        <IconEyeClosed className="h-4 w-4 text-muted-foreground" />
+                      ) : (
+                        <IconEye className="h-4 w-4 text-muted-foreground" />
+                      )}
+                      <span className="sr-only">
+                        {showPassword
+                          ? 'Ocultar contraseña'
+                          : 'Mostrar contraseña'}
+                      </span>
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full flex gap-x-2" onClick={handleLogin}>
+                Iniciar sesión
+              </Button>
+            </CardFooter>
+          </Card>
+        </form>
+      </div>
+      <div className="hidden lg:flex flex-1 items-center justify-center bg-muted">
+        <img
+          src="/illustration-dashboard.webp"
+          alt="Login illustration"
+          width={600}
+          height={600}
+          className="object-cover"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
