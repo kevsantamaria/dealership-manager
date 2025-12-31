8f1e06883d597a578586c65668619213bb31b363
Kevin Santamaria <kevinsdasere@gmail.com>
2025-12-30 19:49:14 -0500

    feat(backend): create supplier controller

---
+++ backend/src/controllers/supplier.controller.ts
@@ -0,0 +1,55 @@
+import { HTTP_STATUS, HTTP_STATUS_MESSAGE } from '@/constants/httpStatus'
+import type { CreateSupplierDTO, UpdateSupplierDTO } from '@/models/dtos/supplier.dto'
+import {
+  createSupplierService,
+  deleteSupplierService,
+  getAllSuppliersService,
+  getSupplierByIdService,
+  updateSupplierService,
+} from '@/services/supplier.service'
+import type { Request, Response } from 'express'
+
+export const createSupplier = async (req: Request, res: Response) => {
+  const supplier: CreateSupplierDTO = req.body
+  const createdSupplier = await createSupplierService(supplier)
+  res.status(HTTP_STATUS.CREATED).json({
+    message: HTTP_STATUS_MESSAGE.CREATED,
+    data: createdSupplier,
+  })
+}
+
+export const getAllSuppliers = async (req: Request, res: Response) => {
+  const suppliers = await getAllSuppliersService()
+  res.status(HTTP_STATUS.OK).json({
+    message: HTTP_STATUS_MESSAGE.OK,
+    data: suppliers,
+  })
+}
+
+export const getSupplierById = async (req: Request, res: Response) => {
+  const { id } = req.params
+  const supplier = await getSupplierByIdService(Number(id))
+  res.status(HTTP_STATUS.OK).json({
+    message: HTTP_STATUS_MESSAGE.OK,
+    data: supplier,
+  })
+}
+
+export const updateSupplier = async (req: Request, res: Response) => {
+  const { id } = req.params
+  const supplier: UpdateSupplierDTO = req.body
+
+  const updatedSupplier = await updateSupplierService(Number(id), supplier)
+  res.status(HTTP_STATUS.OK).json({
+    message: HTTP_STATUS.OK,
+    data: updatedSupplier,
+  })
+}
+
+export const deleteSupplier = async (req: Request, res: Response) => {
+  const { id } = req.params
+  await deleteSupplierService(Number(id))
+  res.status(HTTP_STATUS.OK).json({
+    message: HTTP_STATUS_MESSAGE.OK,
+  })
+}
