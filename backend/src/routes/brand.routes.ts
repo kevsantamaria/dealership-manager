3beaac2b6eed2d83d17e3db646f7ddca75c124d3
Kevin Santamaria <kevinsdasere@gmail.com>
2026-01-11 09:02:30 -0500

    feat(backend): add deleteBrand route

--- backend/src/routes/brand.routes.ts
+++ backend/src/routes/brand.routes.ts
@@ -1,8 +1,11 @@
-import { getAllBrands } from '@/controllers/brand.controller'
+import { deleteBrand, getAllBrands } from '@/controllers/brand.controller'
+import { dtoValidator } from '@/middlewares/dtoValidator.middleware'
+import { idParamDTO } from '@/models/dtos/idParam.dto'
 import { Router } from 'express'
 
 const router = Router()
 
 router.get('/brands', getAllBrands)
+router.delete('/brands/:id', dtoValidator(idParamDTO, 'params'), deleteBrand)
 
 export default router
