c48d2bd66416f3c583aa0b857048da082588e336
Kevin Santamaria <kevinsdasere@gmail.com>
2026-01-11 09:13:53 -0500

    feat(frontend): create hook function for delete brand

--- frontend/src/hooks/useBrands.ts
+++ frontend/src/hooks/useBrands.ts
@@ -1,11 +1,25 @@
-import { fetchBrands } from '@/api/endpoints/brands'
-import { useQuery } from '@tanstack/react-query'
+import { deleteBrand, fetchBrands } from '@/api/endpoints/brands'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 
 export const useBrands = () => {
+  const queryClient = useQueryClient()
+
   const getBrands = useQuery({
     queryKey: ['brands'],
     queryFn: fetchBrands,
   })
 
-  return { getBrands }
+  const deleteBrandById = useMutation({
+    mutationFn: (id: number) => {
+      return deleteBrand(id)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['brands'] })
+    },
+    onError: (error) => {
+      console.error('Error deleting brand:', error)
+    },
+  })
+
+  return { getBrands, deleteBrandById }
 }
