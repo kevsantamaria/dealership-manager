84ddfa75e715707fb74afd9d997a45951319db97
Kevin Santamaria <kevinsdasere@gmail.com>
2026-01-12 15:08:18 -0500

    feat(backend): add getFinancialSummary controller

--- backend/src/controllers/dashboard.controller.ts
+++ backend/src/controllers/dashboard.controller.ts
@@ -1,11 +1,22 @@
-import { HTTP_STATUS, HTTP_STATUS_MESSAGE } from "@/constants/httpStatus"
-import { findVehiclesStockSummary } from "@/repositories/vehicle.repository"
-import type { Request, Response } from "express"
+import { HTTP_STATUS, HTTP_STATUS_MESSAGE } from '@/constants/httpStatus'
+import {
+  getFinancialSummaryService,
+  getVehiclesStockSummaryService,
+} from '@/services/dashboard.service'
+import type { Request, Response } from 'express'
 
-export const getVehiclesStockSummary = async (req:Request, res: Response) => {
-    const summaryVehicles = await findVehiclesStockSummary()
-      res.status(HTTP_STATUS.OK).json({
-        message: HTTP_STATUS_MESSAGE.OK,
-        data: summaryVehicles,
-      })
+export const getVehiclesStockSummary = async (req: Request, res: Response) => {
+  const summaryVehicles = await getVehiclesStockSummaryService()
+  res.status(HTTP_STATUS.OK).json({
+    message: HTTP_STATUS_MESSAGE.OK,
+    data: summaryVehicles,
+  })
+}
+
+export const getFinancialSummary = async (req: Request, res: Response) => {
+  const summary = await getFinancialSummaryService()
+  res.status(HTTP_STATUS.OK).json({
+    message: HTTP_STATUS_MESSAGE.OK,
+    data: summary,
+  })
 }
