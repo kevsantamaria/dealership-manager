a66cb411562ee54c4dc88ec3818fb6f43b6aa988
Kevin Santamaria <kevinsdasere@gmail.com>
2026-01-20 22:30:13 -0500

    feat(frontend): add fetch users endpoint

--- frontend/src/api/endpoints/users.ts
+++ frontend/src/api/endpoints/users.ts
@@ -2,6 +2,17 @@ import api from '@/api/dealership'
 import type { CreateUserPayload } from '@/types/user'
 import { manageError } from '../manageError'
 
+export const fetchUsers = async () => {
+  return api
+    .get('/users')
+    .then((res) => {
+      return res.data.data
+    })
+    .catch((error) => {
+      manageError(error)
+    })
+}
+
 export const addUser = async (user: CreateUserPayload) => {
   return api
     .post('/users', user)
