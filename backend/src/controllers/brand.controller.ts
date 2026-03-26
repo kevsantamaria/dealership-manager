7bb691763d575c6bb55a6431bfa6241684ec18c8
Kevin Santamaria <kevinsdasere@gmail.com>
2026-03-25 21:49:31 -0400

    refactor: migrate brand services to prisma

--- backend/src/controllers/brand.controller.ts
+++ backend/src/controllers/brand.controller.ts
@@ -1,10 +1,12 @@
 import { HTTP_STATUS, HTTP_STATUS_MESSAGE } from '@/constants/httpStatus'
-import { findAllBrandsWithVehicles } from '@/repositories/brand.repository'
-import { deleteBrandService } from '@/services/brand.service'
+import {
+  deleteBrandService,
+  getAllBrandsService,
+} from '@/services/brand.service'
 import type { Request, Response } from 'express'
 
 export const getAllBrands = async (req: Request, res: Response) => {
-  const brands = await findAllBrandsWithVehicles()
+  const brands = await getAllBrandsService()
   res.status(HTTP_STATUS.OK).json({
     message: HTTP_STATUS_MESSAGE.OK,
     data: brands,
--- backend/src/services/brand.service.ts
+++ backend/src/services/brand.service.ts
@@ -1,33 +1,52 @@
-import { pool } from '@/db/pool'
-import type { Brand } from '@/models/entities/brand'
-import {
-  deleteBrand,
-  findAllBrandsWithVehicles,
-  findBrandById,
-  isBrandEmpty,
-} from '@/repositories/brand.repository'
-import { deleteModelsByBrand } from '@/repositories/model.repository'
-import { deleteTrimsByBrand } from '@/repositories/trim.repository'
+import { prisma } from '@/lib/prisma'
 
 export const getAllBrandsService = async () => {
-  const brands: Brand[] = await findAllBrandsWithVehicles()
-  return brands
+  return await prisma.brand.findMany({
+    select: {
+      id: true,
+      name: true,
+      countryOrigin: true,
+      models: {
+        select: {
+          trims: {
+            select: {
+              _count: {
+                select: { vehicles: true },
+              },
+            },
+          },
+        },
+      },
+    },
+    orderBy: {
+      name: 'asc',
+    },
+  })
 }
 
 export const deleteBrandService = async (id: number) => {
-  const existingBrand = await findBrandById(id)
+  const existingBrand = await prisma.brand.findUnique({ where: { id } })
   if (!existingBrand) {
     throw new Error('NOT_FOUND')
   }
-  const notHaveVehicles = await isBrandEmpty(id)
+  const notHaveVehicles = await prisma.vehicle.findFirst({
+    where: {
+      trim: {
+        model: {
+          brandId: id,
+        },
+      },
+    },
+    select: { id: true },
+  })
 
   if (!notHaveVehicles) {
     throw new Error('BRAND_NOT_EMPTY')
   }
 
-  return await pool.transaction(async (tx) => {
-    await deleteTrimsByBrand(id, tx)
-    await deleteModelsByBrand(id, tx)
-    await deleteBrand(id, tx)
+  return await prisma.$transaction(async (tx) => {
+    await tx.trim.deleteMany({ where: { model: { brandId: id } } })
+    await tx.model.deleteMany({ where: { brandId: id } })
+    await tx.brand.delete({ where: { id } })
   })
 }
