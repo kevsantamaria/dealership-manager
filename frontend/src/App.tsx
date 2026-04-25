a0cfd23bdaca59bebc183af708452a6d9ba6a7fb
Kevin Santamaria <kevinsdasere@gmail.com>
2026-04-25 15:01:11 -0400

    fix: moving queryClient instance out of App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
 import AppRouter from './routes/AppRouter'
 
-function App() {
-  const queryClient = new QueryClient()
+const queryClient = new QueryClient()
 
+function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
