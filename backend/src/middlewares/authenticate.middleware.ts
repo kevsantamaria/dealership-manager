afde9ec200034b65b14ab1b6bb59d170b8d5c471
Kevin Santamaria <kevsantamaria01@gmail.com>
2026-07-26 23:33:54 -0400

    fix(backend): auth middleware filename

---
+++ backend/src/middlewares/authenticate.middleware.ts
@@ -0,0 +1,18 @@
+import type { Request, Response, NextFunction } from 'express'
+
+export const authenticate = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { userId } = req.session
+
+  if (!userId || typeof userId !== 'string') {
+    console.warn('[Auth] Unauthorized request from:', req.ip)
+    return res.status(401).json({
+      message: 'NOT_AUTHENTICATED',
+    })
+  }
+
+  next()
+}
